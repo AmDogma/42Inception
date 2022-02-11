8277b0c59c43c7989ae7f258b07b9a0cb0219816
mdewayne <sawden@mail.ru>
2022-02-11 14:51:09 +0300

    first commit

---
+++ Makefile
@@ -0,0 +1,10 @@
+all:
+	docker-compose -f srcs/docker-compose.yml up --build -d
+
+clean:
+	docker-compose -f srcs/docker-compose.yml down
+	docker image prune -a
+
+vclean:
+	docker volume rm $$(docker volume ls -q)
+	rm -rf /home/emil/data/wordpress_data/* /home/emil/data/mariadb_data/*
---
+++ srcs/requirements/wordpress/wp_setup.sh
@@ -0,0 +1,15 @@
+mkdir -p /run/php
+
+while ! mysql -h mariadb -u $DB_USER -p$DB_PW $DB_NAME -e "SELECT 'OK' AS status;"; do
+    sleep 5
+done
+
+if [ ! -f /var/www/wordpress/wp-config.php ]; then
+cp -R /usr/src/wordpress /var/www
+wp core config --path=/var/www/wordpress --dbhost=${DB_HOST} --dbname=${DB_NAME} --dbuser=${DB_USER} --dbpass=${DB_PW} --allow-root
+wp core install --path=/var/www/wordpress --url=${WP_URL} --title=${WP_TITLE} --admin_user=${WP_ADMIN_N} --admin_password=${WP_ADMIN_PW} --admin_email=${WP_ADMIN_MAIL} --skip-email --allow-root
+wp user create --path=/var/www/wordpress --allow-root ${WP_USER} ${WP_USER_EMAIL} --role=author --user_pass=${WP_USER_PW}
+fi
+/usr/sbin/php-fpm7.3 --nodaemonize
+
+
